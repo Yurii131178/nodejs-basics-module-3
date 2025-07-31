3ab3791f2d1fb2a50ce1f145d069526b9ff922e1
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-01 00:06:50 +0300

    creat controller/students.js to remove controllers from routers

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,32 +1,40 @@
 import { Router } from 'express';
-import { getAllStudents, getStudentById } from '../services/students.js';
+// import { getAllStudents, getStudentById } from '../services/students.js';
+
+import {
+  getStudentsController,
+  getStudentByIdController,
+} from '../controllers/students.js';
 
 const router = Router();
 
-router.get('/students', async (req, res) => {
-  const students = await getAllStudents();
+router.get('/students', getStudentsController);
+router.get('/students/:studentId', getStudentByIdController);
 
-  res.status(200).json({
-    data: students,
-  });
-});
+// router.get('/students', async (req, res) => {
+//   const students = await getAllStudents();
 
-router.get('/students/:studentId', async (req, res, next) => {
-  const { studentId } = req.params;
-  const student = await getStudentById(studentId);
+//   res.status(200).json({
+//     data: students,
+//   });
+// });
 
-  // Відповідь, якщо контакт не знайдено
-  if (!student) {
-    res.status(404).json({
-      message: 'Student not found',
-    });
-    return;
-  }
+// router.get('/students/:studentId', async (req, res, next) => {
+//   const { studentId } = req.params;
+//   const student = await getStudentById(studentId);
 
-  // Відповідь, якщо контакт знайдено
-  res.status(200).json({
-    data: student,
-  });
-});
+//   // Відповідь, якщо контакт не знайдено
+//   if (!student) {
+//     res.status(404).json({
+//       message: 'Student not found',
+//     });
+//     return;
+//   }
+
+//   // Відповідь, якщо контакт знайдено
+//   res.status(200).json({
+//     data: student,
+//   });
+// });
 
 export default router;
