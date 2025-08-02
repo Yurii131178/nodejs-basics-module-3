a28dea61bcdd4dfa6ad8532e3eb6534352cbbd87
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-02 09:27:23 +0300

    move request handlers from routers to src/controllers/students.js

--- src/routers/students.js
+++ src/routers/students.js
@@ -2,38 +2,11 @@ import { Router } from 'express';
 import {
   getStudentByIdController,
   getStudentsController,
-} from '../conrollers/students.js';
-// import { getAllStudents, getStudentById } from '../services/students.js';
+} from '../controllers/students.js';
 
 const router = Router();
 
 router.get('/students', getStudentsController);
 router.get('/students/:studentId', getStudentByIdController);
-
-// router.get('/students', async (req, res) => {
-//   const students = await getAllStudents();
-
-//   res.status(200).json({
-//     data: students,
-//   });
-// });
-
-// router.get('/students/:studentId', async (req, res, next) => {
-//   const { studentId } = req.params;
-//   const student = await getStudentById(studentId);
-
-//   // Відповідь, якщо контакт не знайдено
-//   if (!student) {
-//     res.status(404).json({
-//       message: 'Student not found',
-//     });
-//     return;
-//   }
-
-//   // Відповідь, якщо контакт знайдено
-//   res.status(200).json({
-//     data: student,
-//   });
-// });
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -25,32 +25,6 @@ export const startServer = () => {
     }),
   );
 
-  // app.get('/students', async (req, res) => {
-  //   const students = await getAllStudents();
-
-  //   res.status(200).json({
-  //     data: students,
-  //   });
-  // });
-
-  // app.get('/students/:studentId', async (req, res, next) => {
-  //   const { studentId } = req.params;
-  //   const student = await getStudentById(studentId);
-
-  //   // Відповідь, якщо контакт не знайдено
-  //   if (!student) {
-  //     res.status(404).json({
-  //       message: 'Student not found',
-  //     });
-  //     return;
-  //   }
-
-  //   // Відповідь, якщо контакт знайдено
-  //   res.status(200).json({
-  //     data: student,
-  //   });
-  // });
-
   app.use(studentsRouter);
 
   app.use((err, req, res, next) => {
