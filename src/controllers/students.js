fa2fc674518108d7ac40881b804cb2af4d392d18
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-02 10:03:01 +0300

    add next to getStudentByIdController handler to process errors in server.js

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -12,19 +12,56 @@ export const getStudentsController = async (req, res) => {
   });
 };
 
-export const getStudentByIdController = async (req, res) => {
+/**================Обробка помилок====================
+
+// export const getStudentByIdController = async (req, res) => {
+//   const { studentId } = req.params;
+//   const student = await getStudentById(studentId);
+
+//   // Відповідь, якщо контакт не знайдено
+//   if (!student) {
+//     res.status(404).json({
+//       message: 'Student not found',
+//     });
+//     return;
+//   }
+
+//   // Відповідь, якщо контакт знайдено
+//   res.json({
+//     status: 200,
+//     message: `Successfully found student with id ${studentId}!`,
+//     data: student,
+//   });
+// };
+
+/////////////////////////////////////////
+/**================Обробка помилок====================
+У контролері getStudentByIdController функція getStudentById поверне null, якщо не був знайдений студент за вказаним ідентифікатором. Цей випадок обов’язково потрібно опрацювати у коді.
+
+Поки що буде достатньо перевірити наявність значення у змінній student і, якщо там null, викликати next з об'єктом помилки, яка містить повідомлення про те, що студент не був знайдений.
+
+Доповнимо код контролера getStudentByIdController параметром next і створенням об’єкта помилки, якщо студента не знайдено: */
+
+// src/controllers/students.js
+
+export const getStudentByIdController = async (req, res, next) => {
   const { studentId } = req.params;
   const student = await getStudentById(studentId);
 
-  // Відповідь, якщо контакт не знайдено
+  // Код який був до цього
+  // if (!student) {
+  //   res.status(404).json({
+  //     message: "Student not found",
+  //   });
+  //   return;
+  // }
+
+  // А тепер додаємо базову обробку помилки замість res.status(404)
   if (!student) {
-    res.status(404).json({
-      message: 'Student not found',
-    });
+    next(new Error('Student not found'));
     return;
   }
 
-  // Відповідь, якщо контакт знайдено
   res.json({
     status: 200,
     message: `Successfully found student with id ${studentId}!`,
