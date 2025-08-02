b3459819cc1332a091e322a6f578f0be2755cd76
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-02 10:40:16 +0300

    Apply ctrlWrapper to student route handlers to centralize error handling

--- src/routers/students.js
+++ src/routers/students.js
@@ -4,9 +4,19 @@ import {
   getStudentsController,
 } from '../controllers/students.js';
 
+//Створимо у папці src/utils файл ctrlWrapper.js, де оголосимо та експортуємо функцію-обгортку ctrlWrapper.
+
+//Після цього можемо використати цю функцію у роутах для обгортання контролерів.
+
+import { ctrlWrapper } from '../utils/ctrlWrapper.js'; // імпртуємо 'ctrlWrapper'
+
 const router = Router();
 
-router.get('/students', getStudentsController);
-router.get('/students/:studentId', getStudentByIdController);
+// router.get('/students', getStudentsController);
+// router.get('/students/:studentId', getStudentByIdController);
+
+router.get('/students', ctrlWrapper(getStudentsController)); // додаємо функцію-обгортку ctrlWrapper
+
+router.get('/students/:studentId', ctrlWrapper(getStudentByIdController)); // // додаємо функцію-обгортку ctrlWrapper
 
 export default router;
