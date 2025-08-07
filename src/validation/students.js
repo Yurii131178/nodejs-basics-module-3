11918003c5d29e0fe1c4c855c24cecb2d5761e0d
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-07 09:11:40 +0300

    add validation

--- src/validation/students.js
+++ src/validation/students.js
@@ -9,3 +9,21 @@ export const createStudentSchema = Joi.object({
   avgMark: Joi.number().min(2).max(12).required(),
   onDuty: Joi.boolean(),
 });
+
+/**Використання методів валідації: після визначення схеми ви можете використовувати на ній методи валідації, такі як validate або validateAsync, для перевірки об'єктів даних на відповідність цій схемі. */
+
+const dataToValidate = {
+  name: 'John Doe',
+  email: 'john.doe@example.com',
+  age: 12,
+  gender: 'mail',
+  avgMark: 10.2,
+};
+
+const validationResult = createStudentSchema.validate(dataToValidate);
+
+if (validationResult.error) {
+  console.error(validationResult.error.message);
+} else {
+  console.log('Data is valid!');
+}
