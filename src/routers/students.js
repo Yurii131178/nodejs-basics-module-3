f76b1c23e4839b2b48b0bc2c2c0ddf2f8d3b4f6f
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-28 23:35:22 +0300

    update the routers by deleting the prefix /students

--- src/routers/students.js
+++ src/routers/students.js
@@ -8,52 +8,47 @@ import {
   patchStudentController,
 } from '../controllers/students.js';
 
-//Створимо у папці src/utils файл ctrlWrapper.js, де оголосимо та експортуємо функцію-обгортку ctrlWrapper.
-
-//Після цього можемо використати цю функцію у роутах для обгортання контролерів.
-
-import { ctrlWrapper } from '../utils/ctrlWrapper.js'; // імпртуємо 'ctrlWrapper'
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
 import {
   createStudentSchema,
   updateStudentSchema,
-} from '../validation/students.js'; // валідатори схеми з обов'язковими полями і не дуже
-import { isValidId } from '../middlewares/isValidId.js'; // валідатор id
+} from '../validation/students.js';
+
 
 const router = Router();
 
-router.get('/students', ctrlWrapper(getStudentsController)); // додаємо функцію-обгортку ctrlWrapper
+router.get('/', ctrlWrapper(getStudentsController));
 
 router.get(
-  '/students/:studentId',
+  '/:studentId',
   isValidId,
-  ctrlWrapper(getStudentByIdController),
-); // // додаємо функцію-обгортку ctrlWrapper
+  ctrlWrapper(getStudentByIdController)
+);
 
 router.post(
-  // !!! новий роут для створення студентів !!!!!
-  '/students',
-  validateBody(createStudentSchema), // додаємо middleware validationBody пристворенні сутності
+  '/',
+  validateBody(createStudentSchema),
   ctrlWrapper(createStudentController),
 );
 
 router.delete(
-  '/students/:studentId',
+  '/:studentId',
   isValidId,
-  ctrlWrapper(deleteStudentController),
-);
+  ctrlWrapper(deleteStudentController));
 
 router.put(
-  '/students/:studentId',
+  '/:studentId',
   isValidId,
-  validateBody(createStudentSchema), // додаємо middleware validationBody при оновленні сутності
+  validateBody(createStudentSchema),
   ctrlWrapper(upsertStudentController),
 );
 
 router.patch(
-  '/students/:studentId',
+  '/:studentId',
   isValidId,
-  validateBody(updateStudentSchema), // додаємо middleware validationBody при оновленні сутності
+  validateBody(updateStudentSchema),
   ctrlWrapper(patchStudentController),
 );
 
