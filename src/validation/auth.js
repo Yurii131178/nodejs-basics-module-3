491d1d27e5511e72059e84ceb99210fb0c8e003a
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-09-01 01:07:52 +0300

    add resetPasswordSchema

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -24,8 +24,15 @@ export const loginUserSchema = Joi.object({
   password: Joi.string().required(),
 });
 
-// ------reset-----
+// ------request-reset-email-----
 
 export const requestResetEmailSchema = Joi.object({
   email: Joi.string().email().required(),
+});
+
+// ------reset-password-----
+
+export const resetPasswordSchema = Joi.object({
+  password: Joi.string().required(),
+  token: Joi.string().required(),
 });
