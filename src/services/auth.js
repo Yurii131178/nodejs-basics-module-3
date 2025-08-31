ab99919f41423e65cd7fafdd824d334b91f4dcb4
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-08-31 18:47:59 +0300

    install jwt and update auth/server.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -98,11 +98,38 @@ export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
 
 // ----resetToken------
 
+import jwt from 'jsonwebtoken';
+
+import { SMTP } from '../constants/index.js';
+import { getEnvVar } from '../utils/getEnvVar.js';
+import { sendEmail } from '../utils/sendMail.js';
+
+
 export const requestResetToken = async (email) => {
   const user = await UsersCollection.findOne({ email });
   if (!user) {
     throw createHttpError(404, 'User not found');
   }
 
- //доповнимо її трохи пізніше
+  const resetToken = jwt.sign(
+    {
+      sub: user._id,
+      email,
+    },
+    getEnvVar('JWT_SECRET'),
+    {
+      expiresIn: '15m',
+    },
+  );
+
+  await sendEmail({
+    from: getEnvVar(SMTP.SMTP_FROM),
+    to: email,
+    subject: 'Reset your password',
+    html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+  });
+
+
+
+
 };
